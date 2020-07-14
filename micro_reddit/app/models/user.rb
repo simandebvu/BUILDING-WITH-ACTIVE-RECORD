134dd075564801b1afa608db0d6bb96179843ed7
Shingirayi Mandebvu <shingim1@gmail.com>
2020-07-14 17:57:41 +0200

    Add User Model With Validations

---
+++ micro_reddit/app/models/user.rb
@@ -0,0 +1,12 @@
+class EmailValidator < ActiveModel::Validator
+    def validate(record)
+    record.errors[:base] << "Email must have an @ symbol!" unless record.email.include?('@')
+    end
+end
+
+class User < ApplicationRecord
+    validates_with EmailValidator, fields: [:email]
+    validates :username, presence: true, length: { in: 5..25 }, format: { without: /\s/}
+    validates :email,  presence: true, length: { maximum: 100 }, uniqueness: true
+end
+
