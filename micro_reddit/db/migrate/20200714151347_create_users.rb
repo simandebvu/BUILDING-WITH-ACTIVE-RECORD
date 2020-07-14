99ed79ed08a1fafb069e3c4d0b3edc3d2f2b94d9
ricardo <ricardotvera.54@gmail.com>
2020-07-14 12:49:48 -0700

    run rubocop

--- micro_reddit/db/migrate/20200714151347_create_users.rb
+++ micro_reddit/db/migrate/20200714151347_create_users.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.integer :user_id      
+      t.integer :user_id
       t.timestamps
     end
   end
