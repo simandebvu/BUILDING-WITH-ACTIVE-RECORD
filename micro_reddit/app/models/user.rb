5f578bbc1235f222639e95d330011321eb097775
ricardo <ricardotvera.54@gmail.com>
2020-07-14 12:50:17 -0700

    Revert "run rubocop"
    This reverts commit 99ed79ed08a1fafb069e3c4d0b3edc3d2f2b94d9.

--- micro_reddit/app/models/user.rb
+++ micro_reddit/app/models/user.rb
@@ -1,15 +1,14 @@
-# frozen_string_literal: true
-
 class EmailValidator < ActiveModel::Validator
-  def validate(record)
-    record.errors[:base] << 'Email must have an @ symbol!' unless record.email.include?('@')
-  end
+    def validate(record)
+    record.errors[:base] << "Email must have an @ symbol!" unless record.email.include?('@')
+    end
 end
 
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  validates_with EmailValidator, fields: [:email]
-  validates :username, presence: true, length: { in: 5..25 }, format: { without: /\s/ }
-  validates :email, presence: true, length: { maximum: 100 }, uniqueness: true
+    has_many :posts
+    has_many :comments
+    validates_with EmailValidator, fields: [:email]
+    validates :username, presence: true, length: { in: 5..25 }, format: { without: /\s/}
+    validates :email,  presence: true, length: { maximum: 100 }, uniqueness: true
 end
+
--- micro_reddit/db/migrate/20200714163912_remove_user_id_from_user.rb
+++ micro_reddit/db/migrate/20200714163912_remove_user_id_from_user.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class RemoveUserIdFromUser < ActiveRecord::Migration[6.0]
   def change
     remove_column :users, :user_id, :integer
